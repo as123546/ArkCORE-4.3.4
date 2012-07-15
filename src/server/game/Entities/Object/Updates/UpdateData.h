1dfa46fdc761a421731c9f9ec6ee1ef7811aca7a
Venugh <venugh@gmx.net>
2012-07-15 11:39:04 +0200

    Core/Object: Fixed UpdateData for 4.3.4.
    thanks to shauren.

--- src/server/game/Entities/Object/Updates/UpdateData.h
+++ src/server/game/Entities/Object/Updates/UpdateData.h
@@ -30,27 +30,35 @@ class WorldPacket;
 
 enum OBJECT_UPDATE_TYPE
 {
-    UPDATETYPE_VALUES = 0,
-    //UPDATETYPE_MOVEMENT             = 0xFF, //deleted in 4.0.3.
-    UPDATETYPE_CREATE_OBJECT = 1, UPDATETYPE_CREATE_OBJECT2 = 2, UPDATETYPE_OUT_OF_RANGE_OBJECTS = 3,
-//UPDATETYPE_NEAR_OBJECTS         = 0xFF //deleted in 4.0.3.
+    UPDATETYPE_VALUES               = 0,
+    UPDATETYPE_CREATE_OBJECT        = 1,
+    UPDATETYPE_CREATE_OBJECT2       = 2,
+    UPDATETYPE_OUT_OF_RANGE_OBJECTS = 3,
 };
 
 enum OBJECT_UPDATE_FLAGS
 {
-    UPDATEFLAG_NONE = 0x0000, UPDATEFLAG_SELF = 0x0001, UPDATEFLAG_TRANSPORT = 0x0002, UPDATEFLAG_HAS_TARGET = 0x0004,
-    //UPDATEFLAG_LOWGUID      = 0x0008, deleted in 4.0.3
-    //UPDATEFLAG_HIGHGUID     = 0x0010, deleted in 4.0.3
-    UPDATEFLAG_LIVING = 0x0020, UPDATEFLAG_HAS_POSITION = 0x0040, UPDATEFLAG_VEHICLE = 0x0080, UPDATEFLAG_POSITION = 0x0100, UPDATEFLAG_ROTATION = 0x0200, UPDATEFLAG_UNK1 = 0x0400,          //not found.
-    UPDATEFLAG_UNK2 = 0x0800,          //added in 4.0.3
-    UPDATEFLAG_UNK3 = 0x1000,
-//added in 4.0.3
+    UPDATEFLAG_NONE                  = 0x0000,
+    UPDATEFLAG_SELF                  = 0x0001,
+    UPDATEFLAG_TRANSPORT             = 0x0002,
+    UPDATEFLAG_HAS_TARGET            = 0x0004,
+    UPDATEFLAG_UNKNOWN               = 0x0008,
+    UPDATEFLAG_LOWGUID               = 0x0010,
+    UPDATEFLAG_LIVING                = 0x0020,
+    UPDATEFLAG_STATIONARY_POSITION   = 0x0040,
+    UPDATEFLAG_VEHICLE               = 0x0080,
+    UPDATEFLAG_GO_TRANSPORT_POSITION = 0x0100,
+    UPDATEFLAG_ROTATION              = 0x0200,
+    UPDATEFLAG_UNK3                  = 0x0400,
+    UPDATEFLAG_ANIMKITS              = 0x0800,
+    UPDATEFLAG_UNK5                  = 0x1000,
+    UPDATEFLAG_UNK6                  = 0x2000,
 };
 
 class UpdateData
 {
 public:
-    UpdateData ();
+    UpdateData (uint16 map);
 
     void AddOutOfRangeGUID (std::set<uint64>& guids);
     void AddOutOfRangeGUID (const uint64 &guid);
